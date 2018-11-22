48a0044fbacf3769094720773ca9ab48df61ffa6
JonnyBaird <jb@jonathanbaird.co.uk>
2018-11-22 17:01:20 +0000

    finalising styling on index page, job done

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require('mongoose'),
-    methodOverride = require("method-override");
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer");
 
 // App config
 mongoose.connect('mongodb://localhost:27017/restful_blog_app', { useNewUrlParser: true });
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 // Mongoose Model Config
 var blogSchema = new mongoose.Schema({
@@ -44,6 +46,7 @@ app.get("/blogs/new", function(req, res) {
 
 // CREATE ROUTE
 app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
@@ -78,6 +81,7 @@ app.get("/blogs/:id/edit", function(req, res) {
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if(err){
             res.render("index");
